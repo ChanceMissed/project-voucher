afda364d5d7e420f38b63e4a7306da4dbb0575a6
ChanceMissed <bigchancemiss@gmail.com>
2024-07-16 18:07:48 +0900

    [프로젝트 세팅] 리팩토링 - swagger 세팅

--- src/main/java/com/example/project_voucher/app/controller/EmployeeController.java
+++ src/main/java/com/example/project_voucher/app/controller/EmployeeController.java
@@ -20,13 +20,13 @@ public class EmployeeController {
 
     // 회원 생성
     @PostMapping("/api/v1/employee")
-    public Long create(@RequestBody EmployeeCreateRequest request){
+    public Long create(@RequestBody EmployeeCreateRequest request) {
         return employeeService.create(request.name(), request.position(), request.department());
     }
 
     // 회원 조회
     @GetMapping("/api/v1/employee/{no}")
-    public EmployeeResponse get(@PathVariable final Long no) {
+    public EmployeeResponse get(@PathVariable(value = "no") final Long no) {
         return employeeService.get(no);
     }
 }
